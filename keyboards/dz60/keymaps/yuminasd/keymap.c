ed4fc1f6fc371aa1af0721ef82f4765b269b62f5
yuminasd <43660270+yuminasd@users.noreply.github.com>
2021-07-15 18:40:29 -0600

    GuiltyGear Branch

--- keyboards/dz60/keymaps/yuminasd/keymap.c
+++ keyboards/dz60/keymaps/yuminasd/keymap.c
@@ -1,263 +1,111 @@
 #include QMK_KEYBOARD_H
 #define ______ KC_TRNS
 
-static uint16_t key_trigger = 0;
-static uint16_t key_timer = 0;
 
 enum custom_keycodes {
   //MAPLE
-  NORELOAD = SAFE_RANGE,
-  HAMMER,
-  SHOTGUN,
-  SGPSKATE,
-  FORWARDSGP,
-  BACKWARDSGP,
-  VERTICALFARM,
-  HORIZONTALFARM
+  DPRIGHT = SAFE_RANGE,
+  DPLEFT,
+  SUPERRIGHT,
+  SUPERLEFT
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        // M-MACROS
-        case NORELOAD:
-            if (record->event.pressed) {
-                key_trigger = 1;
-            } else {
-                key_trigger = 0;
-            }
-            break;
 
-        case HAMMER:
+        case DPRIGHT:
             if (record->event.pressed) {
                 SEND_STRING(
-                    SS_DOWN(X_N)
-                    SS_DOWN(X_F4)
-                    SS_TAP(X_F3)
-                    SS_DELAY(300)
-                    SS_UP(X_N)
-                    SS_DELAY(100)
-                    SS_UP(X_F4)
-                    SS_DELAY(5)
-                    SS_TAP(X_SCLN)
+                SS_DOWN(X_RIGHT)
+                SS_DELAY(20)
+                SS_DOWN(X_DOWN)
+                 SS_DELAY(20)
+                SS_UP(X_RIGHT)
+                 SS_DELAY(20)
+                SS_DOWN(X_RIGHT)
+                SS_DELAY(20)
+                SS_UP(X_DOWN)
+                 SS_DELAY(20)
+                SS_DOWN(X_F)
+                SS_DELAY(20)
+                SS_UP(X_F)
+                SS_UP(X_RIGHT)
                 );
-            }
+            } 
+            break;
+        
+        case DPLEFT:
+            if (record->event.pressed) {
+                SEND_STRING(
+                SS_DOWN(X_LEFT)
+                SS_DELAY(20)
+                SS_DOWN(X_DOWN)
+                 SS_DELAY(20)
+                SS_UP(X_LEFT)
+                 SS_DELAY(20)
+                SS_DOWN(X_LEFT)
+                SS_DELAY(20)
+                SS_UP(X_DOWN)
+                 SS_DELAY(20)
+                SS_DOWN(X_F)
+                SS_DELAY(20)
+                SS_UP(X_F)
+                SS_UP(X_LEFT)
+                );
+            } 
             break;
 
-        case SHOTGUN:
+          case SUPERRIGHT:
             if (record->event.pressed) {
-                key_trigger = 2;
-            } else {
-                key_trigger = 0;
-            }
+                SEND_STRING(
+                SS_DOWN(X_RIGHT)
+                SS_DELAY(20)
+                SS_DOWN(X_DOWN)
+                SS_DELAY(20)
+                SS_UP(X_RIGHT)
+
+                SS_DOWN(X_LEFT)
+                SS_UP(X_DOWN)
+                SS_DELAY(20)
+
+                SS_DOWN(X_RIGHT)
+                SS_UP(X_LEFT)
+                SS_DELAY(20)
+                SS_UP(X_RIGHT)
+                SS_DOWN(X_F)
+                SS_DELAY(20)
+                SS_UP(X_F)
+                );
+            } 
             break;
 
-        case SGPSKATE:
+              case SUPERLEFT:
             if (record->event.pressed) {
-                key_trigger = 5;
-            } else {
-                key_trigger = 0;
-            }
-            break; 
+                SEND_STRING(
+                SS_DOWN(X_LEFT)
+                SS_DELAY(20)
+                SS_DOWN(X_DOWN)
+                SS_DELAY(20)
+                SS_UP(X_LEFT)
 
-        case FORWARDSGP:
-            if (record->event.pressed) {
-                key_trigger = 6;
-            } else {
-                key_trigger = 0;
-            }
-            break; 
+                SS_DOWN(X_RIGHT)
+                SS_UP(X_DOWN)
+                SS_DELAY(20)
 
-        case BACKWARDSGP:
-            if (record->event.pressed) {
-                key_trigger = 7;
-            } else {
-                key_trigger = 0;
-            }
-            break; 
-
-        case VERTICALFARM:// on release key starts the farm or stops it
-            if (record->event.pressed) {
-            } else {
-                if(key_trigger !=3) {
-                    key_trigger= 3;
-                }
-                else {
-                    key_trigger = 0; 
-                }
-            }
-            break;
-
-        case HORIZONTALFARM:
-            if (record->event.pressed) {
-            } else {
-                if(key_trigger !=4) {
-                    key_trigger= 4;
-                }
-                else {
-                    key_trigger = 0;
-                }
-            }
+                SS_DOWN(X_LEFT)
+                SS_UP(X_RIGHT)
+                SS_DELAY(20)
+                SS_UP(X_LEFT)
+                SS_DOWN(X_F)
+                SS_DELAY(20)
+                SS_UP(X_F)
+                );
+            } 
             break;
     }
     return true;
 };
 
-void matrix_scan_user(void) { 
-
-    switch (key_trigger)
-    {
-        case 1: {//NORELOAD REPEAT
-            if (timer_elapsed(key_timer) > 10) {  // if timer has exceed set amount of time
-                SEND_STRING(SS_TAP(X_F8)SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(200) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN));
-                key_timer = timer_read32();  // reset the timer 
-            }
-        } break;
-
-        case 2: {//SHOTGUN
-            if(timer_elapsed(key_timer)>50) {
-                SEND_STRING(
-                    SS_DOWN(X_F4)
-                    SS_DELAY(200)
-                    SS_TAP(X_DOT)
-                    SS_DELAY(200)
-                    SS_UP(X_F4)
-                    SS_DELAY(5)
-                    SS_TAP(X_SCLN)
-                );
-                key_timer = timer_read32();  // reset the timer 
-            }
-        }break;
-
-        case 3: {//VERTICALFARM
-             if (timer_elapsed(key_timer) > 200) {  
-                SEND_STRING(
-                    SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN)
-                    SS_DELAY(400)
-                    SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN)
-                    SS_DELAY(400)
-                     SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN)
-                    SS_DELAY(400)
-                     SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN)
-                    SS_DELAY(400)
-                    
-                    SS_DOWN(X_N) SS_DOWN(X_F4) SS_DELAY(100) SS_TAP(X_U) SS_DELAY(400) SS_UP(X_N) SS_DELAY(100) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN)
-                    SS_DELAY(1000)
-                   
-                    SS_DOWN(X_RIGHT)
-                    SS_DELAY(480)
-                    SS_UP(X_RIGHT)   
-                    SS_DELAY(500)
-                    SS_DOWN(X_LEFT)
-                    SS_DELAY(80)
-                    SS_UP(X_LEFT)   
-
-                    SS_DELAY(400)
-                    SS_DOWN(X_F4) SS_DELAY(200)SS_TAP(X_DOT)SS_DELAY(200)SS_UP(X_F4)SS_DELAY(5)SS_TAP(X_SCLN)
-                    SS_DELAY(400)
-                    SS_DOWN(X_F4) SS_DELAY(200)SS_TAP(X_DOT)SS_DELAY(200)SS_UP(X_F4)SS_DELAY(5)SS_TAP(X_SCLN)
-                     SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN)
-                    SS_DELAY(400)
-                     SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN)
-                    SS_DELAY(400)
-                );
-                key_timer = timer_read32();  // reset the timer to 
-            }
-        } break;
-
-        case 4: {  // HORIZONTAL FARM
-            if (timer_elapsed(key_timer) > 1000) {
-                SEND_STRING(
-                    // LEFT ONCE
-                    SS_DOWN(X_LEFT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_LEFT) SS_TAP(X_2)
-                        SS_DELAY(300)
-                    SS_DOWN(X_LEFT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_LEFT) SS_TAP(X_1)
-                        SS_DELAY(300)
-                    SS_DOWN(X_LEFT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_LEFT)
-                        SS_DELAY(300)
-                    SS_DOWN(X_LEFT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_LEFT) SS_TAP(X_1)
-                        SS_DELAY(300)
-                                            SS_DOWN(X_LEFT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_LEFT) SS_TAP(X_2)
-                        SS_DELAY(300)
-                    SS_DOWN(X_LEFT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_LEFT) SS_TAP(X_1)
-                        SS_DELAY(300)
-                    SS_DOWN(X_LEFT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_LEFT)
-                        SS_DELAY(300)
-                    SS_DOWN(X_LEFT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_LEFT) SS_TAP(X_1)
-                        SS_DELAY(300)
-
-                    // RIGHT ONCE
-                    SS_DOWN(X_RIGHT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_RIGHT) SS_TAP(X_2)
-                        SS_DELAY(300)
-                    SS_DOWN(X_RIGHT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_RIGHT) SS_TAP(X_1)
-                        SS_DELAY(300)
-                    SS_DOWN(X_RIGHT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_RIGHT)
-                        SS_DELAY(300)
-                    SS_DOWN(X_RIGHT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_RIGHT) SS_TAP(X_1)
-                        SS_DELAY(300)
-                                            SS_DOWN(X_RIGHT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_RIGHT) SS_TAP(X_2)
-                        SS_DELAY(300)
-                    SS_DOWN(X_RIGHT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_RIGHT) SS_TAP(X_1)
-                        SS_DELAY(300)
-                    SS_DOWN(X_RIGHT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_RIGHT)
-                        SS_DELAY(300)
-                    SS_DOWN(X_RIGHT) SS_DELAY(100) SS_DOWN(X_F4) SS_DELAY(200) SS_TAP(X_F1) SS_DELAY(300) SS_TAP(X_F2) SS_DELAY(300) SS_UP(X_F4) SS_DELAY(5) SS_TAP(X_SCLN) SS_DELAY(200) SS_DOWN(X_F4) SS_DELAY(500) SS_UP(X_F4) SS_UP(X_RIGHT) SS_TAP(X_1)
-                        SS_DELAY(300)
-                );
-                key_timer = timer_read32();  // reset the timer to
-            }
-        } break;
-
-        case 5: {
-                if (timer_elapsed(key_timer) > 410) {
-                SEND_STRING(
-                    SS_TAP(X_F4)
-                    SS_DELAY(210)
-                    SS_DOWN(X_SCLN)
-                    SS_TAP(X_QUOT)
-                    SS_UP(X_SCLN)
-                    SS_TAP(X_DOT)
-                );
-                  key_timer = timer_read32();  // reset the timer to
-            }
-            }
-            break;
-
-        case 6: {
-                if (timer_elapsed(key_timer) > 10) {
-                SEND_STRING(
-                    SS_DOWN(X_F4)   
-                    SS_DELAY(150)//50
-                    SS_TAP(X_DOT)
-                    SS_DELAY(150)//50
-                    SS_UP(X_F4)   
-                    SS_DELAY(200)
-                    SS_TAP(X_SCLN)
-                );
-                  key_timer = timer_read32();  // reset the timer to
-            }
-            }
-            break;
-
-            case 7: {
-                if (timer_elapsed(key_timer) > 10) {
-               SEND_STRING(
-                    SS_DOWN(X_F5)   
-                    SS_DELAY(150)//50
-                    SS_TAP(X_DOT)
-                    SS_DELAY(150)//50
-                    SS_UP(X_F5)   
-                    SS_DELAY(200)
-                    SS_TAP(X_SCLN)
-                );
-                  key_timer = timer_read32();  // reset the timer to
-            }
-            }
-            break;
-
-        default:
-        break;
-    }
-}
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -319,12 +167,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | Ctrl |  Cmd  |  Alt  |      Space      |SPC|   Bkspc     |NUMPAD|Rctrl|  L  |  D  |  R  |
    * `-----------------------------------------------------------------------------------------'
    */
-
     LAYOUT_directional(
-      KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, TO(0), TO(0),
-      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-      KC_CAPS, NORELOAD, SHOTGUN, HAMMER, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-      KC_LSFT, SGPSKATE, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
-      FORWARDSGP, BACKWARDSGP, KC_LALT, KC_SCLN, KC_SCLN, KC_BSPC, HORIZONTALFARM, VERTICALFARM , KC_LEFT, KC_DOWN, KC_RIGHT
+      KC_GESC, KC_1, SUPERRIGHT, SUPERRIGHT, SUPERRIGHT, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, TO(0), TO(0),
+      KC_TAB, KC_Q, KC_W, KC_E, KC_R, SUPERLEFT, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+      KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+      KC_LSFT,         KC_Z, KC_X, DPRIGHT, DPLEFT, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
+      KC_LCTL, KC_LGUI, KC_LALT, KC_UP, KC_SPC, KC_UP, KC_UP, KC_RALT , KC_LEFT, KC_DOWN, KC_RIGHT
       ),
+
 };   
